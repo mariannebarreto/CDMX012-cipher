56c7fda9069a68d1f6bcd0a5c239cfec5c31b5ff
mariannebarreto <barretoink@icloud.com>
2022-01-24 20:01:16 -0600

    Lunes (ya llevame diosito)

--- src/cipher.js
+++ src/cipher.js
@@ -3,57 +3,58 @@ const cipher = {encode,decode};
 export default cipher;
 
 //-----------ÁREA CIFRADO--------
+//declaro funciones
+function encode (productName,offset){
+
+
 //variables a utilizar
 let productName= document.getElementById("productInput").value;
-let offset= Number(document.getElementById("numberInput")).value;
-let size= productName.length
-let stringCipher= "";
-const asciiValue = 0;
-const i=0;
+let offset= parseInt(document.getElementById("numberInput")).value;
+let size= productName.length;
+var stringCipher= "";
 
-//declaro funciones
-function encode (productName,offset){;
+for (var i = 0; i<size; i++){  //pasa por cada caracter del string
+let asciiValue= productName.charcodeAt(i); //guarda el valor ASCII de cada caracter
+let cipherFormula;
+let cipherResult;
 
-for(i=0; i<size; i++){// pasa por cada caracter del string
-  asciiValue= productName.charcodeAt(i); //guarda el valor ASCII de cada caracter
+if (asciiValue >= 65 && asciiValue <=90){
+  cipherFormula= (((asciiValue - 65 + offset) % 26) + 65);
+  cipherResult= String.fromCharCode(cipherFormula);
+  stringCipher += cipherResult;
 
-//fórmula para mayúsculas
-if ((asciiValue >= 65) && (asciiValue <=90)){
-  asciiValue = (((asciiValue - 65 + offset) % 26) + 65);
+}else if (asciiValue >= 97 && asciiValue <= 122){
+  cipherFormula = (asciiValue- 97 + parseInt (offset))%26 + 97;
+  cipherResult= String.fromCharCode(cipherFormula);
+  stringCipher += cipherResult;
 }
-//fórmula para minúsculas
-else if ((asciiValue >=97) && (asciiValue <=122)){
-asciiValue = (((asciiValue - 65 + offset) % 26) + 97);
+return stringCipher;
 }
-else {
-  asciiValue = "?";
-}
-stringCipher += String.fromCharCode(asciiValue);
-}
-console.log (cipher)
-//verificar que sirva
-//-------------ÁREA DESCIFRADO-----------------
-
-let codeName= document.getElementById ("codeInput").value
-let size1= codeName.length;
-let stringUncipher= "";
-
-//declaro funciones
-function decode (codeName,offset){; 
-for (i=0; i<size1; i++){// pasa por cada caracter del string
-  asciiValue= codeName.charcodeAt(i); //guarda el valor ASCII de cada caracter
-
-//fórmula para mayúsculas
-if ((asciiValue >= 65) && (asciiValue <=90)){
-  asciiValue = (((asciiValue - 65 + offset) % 26) + 65);
-}
-//fórmula para minúsculas
-else if ((asciiValue >=97) && (asciiValue <=122)){
-asciiValue = (((asciiValue - 65 + offset) % 26) + 97);
-}
-else {
-  asciiValue = "?";
-}
-stringUncipher += String.fromCharCode(asciiValue);
 }
 
+//-----------------ÁREA DESCIFRADO------------
+
+function decode (codeName,offset2){   
+
+//variables a utilizar
+let codeName = document.getElementById ("codeInput").value;
+let offset2 = parseInt(document.getElementById("number_input")).value;
+var stringDecipher ="";
+
+  for(var i = 0; i< myString1.length ; i++){ //pasa por cada carácter del string
+    let ascii_value = codeName.charCodeAt(i); //guarda el valor ASCII de cada caracter
+    let decipherFormula;
+    let decipherResult;
+
+    if (ascii_value  >= 65 && ascii_value <= 90){
+      decipherFormula = ( ascii_value  -13 - parseInt(offset2)) % 26 + 65;
+      decipherResult = String.fromCharCode(decipherFormula);
+      stringDecipher += decipherResult;
+
+    }else if (ascii_value  >= 97 && ascii_value  <=122){
+      decipherFormula = ( ascii_value  - 45 - parseInt(offset2)) % 26 + 97;
+      stringDecipher = String.fromCharCode(decipherFormula);
+      stringDecipher += decipherResult;
+    }
+ return stringDecipher;
+  }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let offset = document.querySelector("numberInput");// almacena valor offset encode
+let offset2 = document.querySelector("number_input");// almacena valor offset decode
+let stringCipher = document.querySelector("productInput");//almacena texto a Cifrar
+let stringDecipher = document.querySelector("codeInput");//almacena texto a descifrar
+
+
+
+//CLICK CIFRADO
+function cipherClick(){
+  let newCode = cipher.encode(offset.value,stringCipher.value);
+  document.getElementById("cipherOut").innerText= newCode;
+
+}
+document.getElementById("cipher-button").onclick=function() {cipherClick()};
+
+//CLICK DESCIFRADO
+
+function decipherClick(){
+  let newProduct = cipher.decode(offset2.value,stringDecipher.value);
+  document.getElementById("decipher").innerText= newProduct;
+
+}
+document.getElementById("decipher-button").onclick=function() {decipherClick()};
